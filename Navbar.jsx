b99207ac859acb8c9df6e9053645c142db3501a4
nikolyar000 <144709259+nikolyar000@users.noreply.github.com>
2025-09-09 01:24:46 -0700

    Start

---
+++ Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logo from "../../assets/logo.png"
+
+const Navbar = () => {
+  return (
+    <div>
+        {/* upper Navbar */}
+        <div>
+            <div>
+                <div>
+                    <a href="#">
+                        <img src={Logo} alt="Logo" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        {/* lower Navbar */}
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
