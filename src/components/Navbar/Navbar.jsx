4ceb8bdaf098c087872ea8a373a8769cde75ad3f
Yaroslav <nikolyar000>
2025-09-11 18:40:32 -0700

    Main

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
                     <img src={Logo} alt="Logo" className="w-10"/>
                 </a>
             </div>
-            {/* search bar and order button */}
-            <div>
+            {/* search bar */}
+            <div className="flex justify-between items-center gap-4">
                 <div className="relative group hidden sm:block">
                     <input type="text" placeholder="search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"/>
                     <IoMdSearch
