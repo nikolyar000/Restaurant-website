c98c00a9a97e7a5281f673f9177a7bc538d9b0d7
1 <nikolyar000battlegrounds.org>
2025-09-12 03:04:55 -0700

    1

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
-const DarkMode = () => {
+const DarkMode = () => {1
   const [theme, setTheme] = React.useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
