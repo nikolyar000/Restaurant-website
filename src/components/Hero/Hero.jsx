ed256ea7804d284e56e7c291bdc9eaf49f69ecb3
nikolyar000 <y1798235@gmail.com>
2025-09-14 18:49:45 -0700

    Hero-update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image1 from "../../assets/hero/women.png"
 import Image2 from "../../assets/hero/shopping.png"
 import Image3 from "../../assets/hero/sale.png"
+import Slider from "react-slick";
 
 const ImageList = [
   {
@@ -28,22 +29,37 @@ const ImageList = [
 ];
 
 const Hero = () => {
+
+    var settings = {
+        dots: false,
+        arrows: false,
+        infunite: true,
+        speed: 800,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease-in-out",
+        pauseOnHover: false,
+        pauseOnFocus: true,
+    }
+
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
         {/* background pattern */}
         <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45-z-9"></div>
         {/* hero section */}
         <div className="container pb-8 sm:pb-0">
-            <div>
+            <Slider {...settings}>
+                {ImageList.map((data) => (
+                <div>
                 <div className="grid-cols-1 sm:grid-cols-2">
                     {/* text content section */}
-                    <div>
+                    <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
                         <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Lorem ipsum dolor sit.
-
+                            {data.title}
                         </h1>
                         <p className="text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, repellat 
-                            ipsum!
+                            {data.description}
                         </p>
                         <div>
                             <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 founded-full">
@@ -51,9 +67,21 @@ const Hero = () => {
                             </button>
                         </div>
                     </div>
-                    {/* text content section */}
+                    {/* image section */}
+                    <div className="order-1 sm:order-2">
+                        <div className="relative z-10">
+                            <img 
+                            src={data.img} 
+                            alt=""
+                            className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto" 
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
+                ))}
+            
+            </Slider> 
         </div>
     </div>
   )
